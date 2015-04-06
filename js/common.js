9248a34528e61951bce9c795c54fc1a8f0f44a1d
moroz <arturmorozv@gmail.com>
2015-04-06 19:05:55 +0300

    added some pages

--- js/common.js
+++ js/common.js
@@ -1,15 +1,53 @@
 head.ready(function() {
 
+// slider
+(function () {
+	var sl = $('.js-sl');
+	if (sl.length) {
+		sl.each(function () {
+			$(this).slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				autoplay: true,
+				autoplaySpeed: 3000,
+				arrows: false,
+				slides: '.sl__item',
+				infinite: true,
+				speed: 500,
+				fade: true,
+				cssEase: 'linear',
+				swipe: false,
+				pauseOnHover: false
+			});
+		});
+	};
+}());	
+
+// slider with arrows
+(function () {
+	var sl = $('.js-slider');
+	if (sl.length) {
+		sl.each(function () {
+			$(this).slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				slides: '.slider__item'
+			});
+		});
+	};
+}());	
+
 // select
 (function () {
 	var el = $('.js-select');
 	if (el.length) {
 		el.each(function () {
 			el.find('select').on('change', function () {
-		    var thisEl = $(this),
-		    	value = $(this).val();
-		    thisEl.prev().html(value);
-		});
+			    var select = $(this),
+			    	value = $(this).val();
+			    select.prev().html(value);
+			    select.parent().addClass('is-active');
+			});
 		});
 	};
 }());
